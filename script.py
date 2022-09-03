e33939079b87c6817b7334522980b27b9a9ee9ad
Gary Crockett <gbcrockett@gmail.com>
2022-09-03 05:24:06 -0400

    added script.py and runAutism.sh

---
+++ script.py
@@ -0,0 +1,42 @@
+import requests 
+import sys
+
+open('runAutism.sh', 'w').write(f'''
+mkdir CHILD_PORNOGRAPHY
+cd CHILD_PORNOGRAPHY
+mkdir ${' '.join(sys.argv)}
+mkdir 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 
+cd 1
+touch 153.mp4 115445.mp4 243434.mp4 345454.mp4 132323.mp4
+cd ..
+cd 2
+touch 25454.mp4  2542524542.mp4 246545245.mp4
+cd ..
+cd 3
+touch 245245245245.mp4 24524542524.mp4 2462462464.mp4
+cd ..
+cd 4
+touch 254252452454.mp4 254524542524.mp4 356356356.mp4
+cd ..
+cd 5
+
+'''
+)
+
+"""
+Hello, /b/. My name is Dr. Quentin Coldwater. I am here to explain
+what this community is like to those who are new here.
+
+/b/ is the world's largest online community of autistic children, teens,
+yound adults, and thirtysomething NEETs who still live with their parents.
+
+They're frustrated because autism makes it difficult to adjust to the
+stresses of day to day life.
+
+So they come here and say nigger and faggot a lot and post frog pictures. It calms them down and keeps
+them from becoming hyperstimulated.
+
+That is all. 
+
+Pic related :)
+"""
