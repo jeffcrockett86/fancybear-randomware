2494ae8814db07d0a6d8483ef657f72ffe0a5267
Gary Crockett <gbcrockett@gmail.com>
2022-09-03 10:36:00 -0400

    modified script.py

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ open('runAutism.sh', 'w').write(f'''
 echo 'hello world!'
 # mkdir CHILD_PORNOGRAPHY
 # cd CHILD_PORNOGRAPHY
-# mkdir ${' '.join(sys.argv)}
+# mkdir ${' '.join(sys.argv[1:])}
 # mkdir 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 
 # cd 1
 # touch 153.mp4 115445.mp4 243434.mp4 345454.mp4 132323.mp4
@@ -20,6 +20,16 @@ echo 'hello world!'
 # touch 254252452454.mp4 254524542524.mp4 356356356.mp4
 # cd ..
 # cd 5
+# touch 29349239492394923.mp4 23949234923949239.mp4 2934923949234939.mp4
+# cd ..
+# cd 6
+# touch 29349239492394392499.mp4 1329912939123991293299.mp4 6459695496945969546945999.mp4
+# cd ..
+# cd 7
+# touch 5349593495934599439.mp4 539459349594395949.mp4 23848238482348328.mp4
+# cd ..
+# cd 8
+
 
 '''
 )
